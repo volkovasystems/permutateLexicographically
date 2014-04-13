a0f9fdee64d0f0811a8885f73b57e24bb376b80b
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-04-13 18:42:19 +0800

    Added permutateLexicographically procedure.

---
+++ permutateLexicographically.java
@@ -0,0 +1,22 @@
+package permutateLexicographically;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+
+import calculatePartition.calculatePartition;
+import convertToSequence.convertToSequence;
+
+public class permutateLexicographically{
+	public static void main( String... parameters ){
+		/*
+			Parameters: 
+				dictionary
+				range, starting and ending range
+
+		*/
+	}
+
+	public static final HashMap<BigInteger, String> permutateLexicographically( String startingRange, String endingRange, String, dictionary, String separator ){
+
+	}
+}
