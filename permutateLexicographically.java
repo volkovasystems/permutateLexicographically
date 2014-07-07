214cf69e75d775bde62df06fc3c34296d205c50c
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-07-07 10:56:31 +0800

    Cleaned things up and modify the way we output the results. Updated convertToSequence sub module.

--- permutateLexicographically.java
+++ permutateLexicographically.java
@@ -1,16 +1,16 @@
 package permutateLexicographically;
 
 import java.math.BigInteger;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.Set;
+import java.util.Map;
+import java.util.Arrays;
 
 import static convertToSequence.convertToSequence.convertToSequence;
+
 /*
 	NOTE: Always compile with '-d .' 
 		And always run with <package-name>.<class-name> format
 */
-
 public class permutateLexicographically{
 	public static final String EMPTY_STRING = "";
 
@@ -31,48 +31,31 @@ public class permutateLexicographically{
 			separator = parameters[ 3 ];
 		}
 
-		LinkedHashMap <BigInteger, String> sequenceList = null;
+		Map <BigInteger, String> sequenceList = null;
 		try{
 			sequenceList = permutateLexicographically( startingIndex, endingIndex, dictionary, separator );
 		}catch( Exception exception ){
 			System.err.print( exception.getMessage( ) );
 			return;
 		}
-                //code for lexicographically sort all genereted strings irregardless of length
-//                Collection <String> sequenceResultList = sequenceList.values();
-//                List< String > results = new ArrayList< String >( sequenceResultList );
-//                Collections.sort(results);
-//                
-//                for( String sequence:results ){
-//                    System.out.println( sequence );
-//                }	
 
-                //code for lexicographically sort all genereted strings with regards to length
-                
-                Set entrySet = sequenceList.entrySet();
-                Iterator iterate = entrySet.iterator();
-                
-                System.out.println("LinkedHashMap entries : ");
-                
-                while(iterate.hasNext())
-                    System.out.println(iterate.next());
-        }
+        String[ ] sequenceListArray = sequenceList.values( ).toArray( new String[ sequenceList.size( ) ] );
+        System.out.print( Arrays.toString( sequenceListArray ).replaceAll( "^\\[|\\]$", "" ) );
+	}
 
-	public static final LinkedHashMap <BigInteger, String> permutateLexicographically( String startingIndex, String endingIndex, String dictionary, String separator )
+	public static final Map <BigInteger, String> permutateLexicographically( String startingIndex, String endingIndex, String dictionary, String separator )
 		throws Exception
 	{	
 		BigInteger startIndex = new BigInteger( startingIndex );
 		BigInteger endIndex = new BigInteger( endingIndex );
 		LinkedHashMap <BigInteger, String> sequenceList = new LinkedHashMap<>( );
-		
+
 		if( endIndex.compareTo( startIndex ) == 1 ){
 			BigInteger currentIndex = new BigInteger( startIndex.toString( ) );
-			
 
 			do{
 				String sequence = convertToSequence( currentIndex.toString( ), dictionary, separator );
-				//System.out.println(sequence);
-                                sequenceList.put( currentIndex, sequence );
+				sequenceList.put( currentIndex, sequence );
 
 				currentIndex = currentIndex.add( BigInteger.ONE );
 			}while( currentIndex.compareTo( endIndex ) <= 0 );
